7a3f766a907bba534cbb485e3bda5f1ad008f466
Joel-Adams <jadams111686@gmail.com>
2020-06-05 14:04:08 -0400

    Update run-vsftpd.sh

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If no env var for FTP_USER has been specified, use 'admin':
+if [ "$FTP_USER" = "**String**" ]; then
+    export FTP_USER='admin'
+fi
+
 # If no env var has been specified, generate a random password for FTP_USER:
 if [ "$FTP_PASS" = "**Random**" ]; then
     export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
